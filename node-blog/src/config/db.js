911daf13168011d98a2f86f35738f9403dc10fc1
Ryan Ren <penghaoren7@gmail.com>
2024-05-19 23:17:50 +1000

    integrated mysql in node.js

---
+++ node-blog/src/config/db.js
@@ -0,0 +1,25 @@
+const env = process.env.NODE_ENV;
+let MYSQL_CONFIG;
+
+if (env === "dev") {
+  MYSQL_CONFIG = {
+    host: "localhost",
+    user: "root",
+    password: "rphqgmebrF1",
+    port: "3306",
+    database: "blog",
+  };
+}
+if (env === "production") {
+  MYSQL_CONFIG = {
+    host: "localhost",
+    user: "root",
+    password: "rphqgmebrF1",
+    port: "3306",
+    database: "blog",
+  };
+}
+
+module.exports = {
+  MYSQL_CONFIG,
+};
---
+++ node-blog/src/db/mysql.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql");
+const { MYSQL_CONFIG } = require("../config/db");
+
+const connection = mysql.createConnection(MYSQL_CONFIG);
+connection.connect();
+
+//function to execute sql query sentence
+const execSql = (sql) => {
+  return new Promise((resolve, reject) => {
+    connection.query(sql, (err, result) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(result);
+    });
+  });
+};
+
+module.exports = {
+  execSql,
+};
